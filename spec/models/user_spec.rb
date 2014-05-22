fc4aeaa65d4c83fa6f7dc825087c5e0f069c1116
Zack Sheppard <z@zack.io>
2014-05-22 14:56:28 -0400

    Create basic user model and password auth

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,14 +2,20 @@ require 'spec_helper'
 
 describe User do
 
-	before { @user = User.new(name: "Example User", email: "user@example.com")}
+	before do 
+		@user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
+	end
+
 	subject { @user }
 
 	it { should be_valid }
-
-	# Name tests
 	it { should respond_to(:name)}
+	it { should respond_to(:email)}
+	it { should respond_to(:password_digest) }
+	it { should respond_to(:password) }
+	it { should respond_to(:password_confirmation) }
 
+	#=========NAME TESTS==========
 	describe "when name is not present" do
 		before { @user.name = " "}
 		it { should_not be_valid}
@@ -25,9 +31,46 @@ describe User do
 		it {should_not be_valid}
 	end
 
-	#Email tests
-	it { should respond_to(:email)}
 
+	#=======PASSWORD TESTS=========
+	describe "when password is not present" do
+		before do @user = User.new(name: "Example User", email: "user@example.org", password: " ", password_confirmation: " ")
+		end
+		it { should_not be_valid }
+	end
+
+	describe "when password does not match" do
+		before do @user = User.new(name: "Example User", email: "user@example.org", password: "123456", password_confirmation: "456123")
+		end
+		it { should_not be_valid }
+	end
+
+	it { should respond_to(:authenticate) }
+
+	describe "with a password that's too short" do
+		before { @user.password = @user.password_confirmation = "a" * 5 }
+		it {should be_invalid}
+	end
+
+	describe "return value of authenticate method" do
+		before { @user.save }
+		let(:found_user) { User.find_by(email: @user.email)}
+
+		describe "with valid password" do
+			it {should eq found_user.authenticate(@user.password)}
+		end
+
+		describe "with invalid password" do
+			let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+			it { should_not eq user_for_invalid_password }
+			specify { expect(user_for_invalid_password).to be_false }
+		end
+	end
+
+
+
+	#=======EMAIL TESTS=========
 	describe "when email is not present" do
 		before { @user.email = " "}
 		it { should_not be_valid}
